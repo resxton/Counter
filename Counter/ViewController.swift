2e77e962ae40bc8684c3909526a91bf169a23d26
resxton <gornomirov661@gmail.com>
2024-11-01 01:03:27 +0300

    Basic task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var counterLabel: UILabel!
+    @IBOutlet var counterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonTapped() {
+        if let oldValue = Int(counterLabel.text ?? "") {
+            let newValue = oldValue + 1
+            counterLabel.text = String(newValue)
+            print("\(oldValue) changed to \(newValue)")
+        } else {
+            print("There was a problem with unwrapping text")
+        }
+    }
 }
 
